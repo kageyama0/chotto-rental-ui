fc1c95cb3cbfc4f200e450c19484172392f84139
yuki kageyama <kageyamaforwork@gmail.com>
2024-12-20 09:07:07 +0900

    add css for @mantine/dates

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,6 +4,7 @@ import { theme } from '@/app/theme';
 import { queryClient } from '@/services/api/apiClient';
 import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
 import { QueryClientProvider } from '@tanstack/react-query';
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
