9d3944a0f4f0389f42fdcb591c99349c3b47ddff
yuki kageyama <kageyamaforwork@gmail.com>
2024-12-23 20:57:57 +0900

    SVGのpropsをキャメルケースに

--- src/components/base/Icon/IconClose.tsx
+++ src/components/base/Icon/IconClose.tsx
@@ -14,9 +14,9 @@ export const IconClose = memo(
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className="icon icon-tabler icons-tabler-outline icon-tabler-square-rounded-x"
       >
         <title>Close</title>
